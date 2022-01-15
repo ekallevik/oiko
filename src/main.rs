fadaed631f7e2bb67ef02e2ed7a4430a72ace146
Even Kallevik <even.kallevik@gmail.com>
2022-01-15 18:49:37 +0100

    Add simple parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use crate::parser::parse_csv;
+
+mod parser;
+
 fn main() {
     println!("Hello, world!");
+
+    let transactions = parse_csv("./Siste transaksjoner.txt").unwrap();
+    for transaction in transactions {
+        println!("{}", transaction)
+    }
 }
